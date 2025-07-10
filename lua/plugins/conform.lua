6bdd67e9b4a82a95e8798cab1d67aef22ea63a3b
Jayesh Nalwa <nalwajayesh97@gmail.com>
2025-07-11 00:19:25 +0530

    python support added

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -23,7 +23,7 @@ return {
 			rust = { "rustfmt", lsp_format = "fallback" },
 			go = { "gofmt", lsp_format = "fallback" },
 			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
+			python = { "ruff_format", "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			javascript = { "prettierd", "prettier", stop_after_first = true },
